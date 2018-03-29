a9ce8d99ae168410269e3611a64adfd741c3b44a
Freek de Haas <freekdh@gmail.com>
2018-03-28 19:40:48 -0700

    Automated nice plots

--- DataAnalysisAutomated.R
+++ DataAnalysisAutomated.R
@@ -4,6 +4,11 @@ args <- commandArgs(trailingOnly = TRUE)
 Foldername <- args[1]
 
 setwd(Foldername)
+library(grid)
+library(gridGraphics)
+library(gtable)
+library(gridExtra)
+library(lattice)
 
 list.data<-list()
 
@@ -42,18 +47,47 @@ ColorRamp <- rgb( seq(1,1,length=256),  # Red
                     seq(1,0,length=256),  # Green
                     seq(0,0,length=256))  # Blue
 
-pdf("PlotData.pdf")
-par(mfrow=c(1,1), mar = c(0,0,0,0))
-image(B,col = ColorRamp)
-text(0.1, 0.95, paste("r0 = ", pars$value[1]))
-text(0.1, 0.9, paste("r1 = ", pars$value[2]))
-text(0.1, 0.85, paste("k0 = ", pars$value[3]))
-text(0.1, 0.8, paste("k1 = ", pars$value[4]))
-par(mfrow=c(NMETA,1), mar = c(0,0,0,0), new = TRUE)
+ColorLevels <- seq(0, 1, length=length(ColorRamp))
+
+
+par(mfrow=c(1,1), cex=1.5 , mar = c(0,0,0,0))
+image(1:nrow(B),1:ncol(B),B,col = ColorRamp)
+legend("topleft", legend = c(paste("r0 = ", pars$value[1]),
+                            paste("r1 = ",pars$value[2]),
+                            paste("k0 = ", pars$value[3]),
+                            paste("k1 = ",pars$value[4]),
+                            paste("n01 = ",pars$value[5]),
+                            paste("n02 = ",pars$value[6]),
+                            paste("rep = ", pars$value[10])), 
+                  bty = "n")
+par(mfrow=c(NMETA,1), mar = c(0,0,0,0))
 for(i in 1:NMETA-1){
     par(mfg=c(i+1,1))
     plot(list.data[[NMETA-i]]$Total, type = 'l',xaxt='n', yaxt='n', xaxs = "i",yaxs="i" , lwd = 3,
-    ylim = c(0,1.15*max(pars$value[3], pars$value[4])*pars$value[10]),
+    ylim = c(0,1.05*max(pars$value[3], pars$value[4])*pars$value[10]),
     bty="n")
 }
+
+grid.echo()
+a <- grid.grab()
+grid.newpage()
+pushViewport(viewport(y=0.08, height = .9, width = .825, just = "bottom"))
+grid.draw(a)
+a <- grid.grab()
+dev.off()
+
+par(cex=1.5)
+image(1, ColorLevels,
+      matrix(data=ColorLevels, ncol=length(ColorLevels),nrow=1),
+      col=ColorRamp,
+      xlab="",ylab="",
+      xaxt="n")
+
+grid.echo()
+b <- grid.grab()
+dev.off()
+pdf("GraphPDF.pdf",width = 15, height = 10)
+grid.arrange(a, b, ncol=2, widths = c(10,1),
+               top=textGrob("Title", gp=gpar(fontsize=20,font=8)))
+grid.rect(gp=gpar(fill=NA))
 dev.off()
